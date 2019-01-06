04b07f230316cdb9030f4d5decdd9d73c5a292ba
Simon Pang <zppang@SimondeMacBook-Pro.local>
2019-01-06 20:22:03 +0800

    upload artical 4 exercise

--- src/golang-Learning/article4/q3/demo9.go
+++ src/golang-Learning/article4/q3/demo9.go
@@ -1,1 +1,16 @@
-package q3
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	var err error
+	n, err := io.WriteString(os.Stdout, "Hello everone! \n") //这里对err进行了重声明
+	if err != nil {
+		fmt.Printf("ERROR: %V\n", err)
+	}
+	fmt.Printf("%d byte(s) were written.\n", n)
+}
