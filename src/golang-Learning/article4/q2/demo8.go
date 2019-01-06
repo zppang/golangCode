3693fc56f20923430d2febda4f0e322ccbab286c
Simon Pang <zppang@SimondeMacBook-Pro.local>
2019-01-06 20:15:37 +0800

    upload artical 4 exercise

--- src/golang-Learning/article4/q2/demo8.go
+++ src/golang-Learning/article4/q2/demo8.go
@@ -1,1 +1,22 @@
-package q2
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	var name = getTheFlag()
+	flag.Parse()
+	fmt.Printf("Hello, %v!\n", *name)
+}
+
+/*func getTheFlag() *string {
+	return  flag.String("name", "everyone", "The greeting object")
+}*/
+
+
+//上面函数的实现也可以是这样的。 在不改变主程序的情况下，便于对程序（如函数）的重构
+func getTheFlag() *int {
+	return  flag.Int("name", 1, "The greeting object")
+}
